8193042eecc623986d103a1f15734c7d034f68c7
Bekhzad Khamidullaev <bekhzad_94@icloud.com>
2023-09-12 17:00:11 +0500

    last changes xz

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
-    path('kanban/', views.products, name='kanban'),
+    path('product_detail/<int:pk>', views.product_detail, name='product_detail'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Products
 
 def products(request):
-    return render(request, 'products.html')
+    products = Products.objects.all()
+    return render(request, 'products.html', {'products': products})
 
-def kanban(request):
-    return render(request, 'kanban.html')
+
+def product_detail(request):
+    product_detail = Products.objects.all()
+    return render(request, 'products_detail.html', {'product': product_detail})
